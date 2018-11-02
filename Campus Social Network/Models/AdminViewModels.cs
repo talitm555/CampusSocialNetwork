85942d1b9850091ccf99931dd99ef508e45d7b36
Talit Mahmood <talitm555@gmail.com>
2018-11-02 23:16:35 +0500

    Layouts, Admin Views, Database Updated.

---
+++ Campus Social Network/Models/AdminViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Campus_Social_Network.Models
+{
+    public class AdminViewModels
+    {
+        public class AddStudent
+        {
+
+        }
+    }
+    
+}
